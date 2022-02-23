1050e3b2081c4bd5625df7b0ad6baf6313a06f58
peter.aruldass <peter.aruldass@CDC2-L-JK4SWZ2.dir.svc.accenture.com>
2022-02-23 18:28:54 +0530

    changes made to the main method

--- NewJava/src/NewClass.java
+++ NewJava/src/NewClass.java
@@ -3,5 +3,6 @@ public class NewClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi. how are you doing?");
+		System.out.println("I have made some changes to the code");
 	}
 }
