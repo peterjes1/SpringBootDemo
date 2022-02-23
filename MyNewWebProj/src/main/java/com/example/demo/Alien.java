7a80934c51eb98d1d4cb239e5dc2f86d2ae6b1ee
peter.aruldass <peter.aruldass@CDC2-L-JK4SWZ2.dir.svc.accenture.com>
2022-02-23 18:53:19 +0530

    commit1

---
+++ MyNewWebProj/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+public class Alien {
+	
+	int aId;
+	String aName;
+	String aLang;
+	public int getaId() {
+		return aId;
+	}
+	public void setaId(int aId) {
+		this.aId = aId;
+	}
+	public String getaName() {
+		return aName;
+	}
+	public void setaName(String aName) {
+		this.aName = aName;
+	}
+	public String getaLang() {
+		return aLang;
+	}
+	public void setaLang(String aLang) {
+		this.aLang = aLang;
+	}
+	
+
+}
---
+++ MyNewWebProj/src/main/java/com/example/demo/MyNewWebProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNewWebProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyNewWebProjApplication.class, args);
+	}
+
+}
