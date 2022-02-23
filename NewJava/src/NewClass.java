39e463e9ca9d49d1c0c3c7a4e509aba4346745fc
peterjes1 <98931677+peterjes1@users.noreply.github.com>
2022-02-23 18:44:25 +0530

    Delete NewJava directory

--- NewJava/src/NewClass.java
+++
@@ -1,8 +0,0 @@
-
-public class NewClass {
-
-	public static void main(String[] args) {
-		System.out.println("Hi. how are you doing?");
-		System.out.println("I have made some changes to the code");
-	}
-}
